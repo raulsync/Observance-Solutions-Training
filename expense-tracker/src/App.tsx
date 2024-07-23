44ff3b27c377d2ba4c819f99f60f2bd3b248aa12
raulsync <duffer.rdx@gmail.com>
2024-07-23 17:42:07 +0530

    initial-setup

---
+++ expense-tracker/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import ExpenseForm from './components/expenseInput/ExpenseForm';
+
+function App() {
+  return (
+    <>
+      <div className="expense-tracker">
+        <h1>Expense Tracker</h1>
+      </div>
+      <ExpenseForm />
+    </>
+  );
+}
+
+export default App;
