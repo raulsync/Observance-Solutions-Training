f528f43efbc8dde2676d0c702ec66a5e5d28b8d7
raulsync <duffer.rdx@gmail.com>
2024-07-29 18:48:47 +0530

    initial-setup

---
+++ book-store/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <>
+      {/* <h1>Book store</h1> */}
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ book-store/src/components/header/Header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <div className="nav-bar">
+      <nav>
+        <div className="logo  w-48 h-12">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Open_Library_logo.svg/768px-Open_Library_logo.svg.png"
+            alt="store-logo"
+            className="logo w-48 h-12"
+          />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
