a536fb0546a1f996426c97680152417dedf8ffb2
raulsync <duffer.rdx@gmail.com>
2024-08-03 08:58:49 +0530

    router_booklist_detail

--- book-store/src/App.tsx
+++ book-store/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './pages/Home/Home';
+import BookListPage from './pages/booksListPage/BookListPage';
+import BookDetails from './components/bookDetails/BookDetails';
 
 function App() {
   return (
@@ -11,6 +13,14 @@ function App() {
         <Route
           path="/"
           element={<Home />}
+        />
+        <Route
+          path="/book"
+          element={<BookListPage />}
+        />
+        <Route
+          path="/book/:id"
+          element={<BookDetails />}
         />
       </Routes>
     </BrowserRouter>
--- book-store/src/api/searchApi.ts
+++ book-store/src/api/searchApi.ts
@@ -6,10 +6,13 @@ export const fetchSearchBooks = async (query: string) => {
   try {
     const response = await axios.get(URL, {
       params: {
-        q: query,
+        title: query,
+        limit: 10,
       },
     });
     console.log(response.data);
+    // const jsonData = await response.json();
+    // return jsonData;
     return response.data;
   } catch (error) {
     console.log('api error occured', error.message);
