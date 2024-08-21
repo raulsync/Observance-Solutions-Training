edf002fe91b91b01c80ba2c84c122d032ef41d2b
raulsync <duffer.rdx@gmail.com>
2024-08-21 11:45:13 +0530

    autocomplet-date-picker-done

--- react-component-practise/src/App.jsx
+++ react-component-practise/src/App.jsx
@@ -1,7 +1,36 @@
-import AutoSuggestion from './components/AutoComplete';
+import { useState } from 'react';
+import { AutoComplete } from 'primereact/autocomplete';
+import { Calendar } from 'primereact/calendar';
 
 function App() {
-  return <AutoSuggestion />;
+  const [value, setValue] = useState('');
+  const [items, setItems] = useState([]);
+
+  const [date, setDate] = useState(null);
+
+  const search = (event) => {
+    setItems([...Array(10).keys()].map((item) => event.query + '-' + item));
+  };
+
+  return (
+    <div className="shadow-xl flex flex-col mt-4 w-[100%]  h-[100vh] items-center justify-center gap-4">
+      <h1 className="font-bold">AutoComplete</h1>
+      <AutoComplete
+        value={value}
+        suggestions={items}
+        completeMethod={search}
+        onChange={(e) => setValue(e.value)}
+        placeholder="Search"
+        className="border border-black "
+      />
+      <Calendar
+        value={date}
+        onChange={(e) => setDate(e.value)}
+        className="border border-gray-400"
+        placeholder="datepicker"
+      />
+    </div>
+  );
 }
 
 export default App;
